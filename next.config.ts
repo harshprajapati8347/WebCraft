67a0864ddb8b67a09a2efde815116748fcb3dd82
Harsh Prajapati <harshprajapati8347@gmail.com>
2026-06-19 16:37:42 +0530

    feat: initialize Next.js project with shadcn/ui

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: [
+    "@cline/sdk",
+    "@cline/core",
+    "@cline/agents",
+    "@cline/llms",
+    "@cline/shared",
+  ],
+  allowedDevOrigins: [
+    "http://localhost:3000",
+    "192.168.1.4"
+  ]
 };
 
 export default nextConfig;
